8bedec9cae7e550fae965b80b947cb9972f76007
José F. Romaniello <jfromaniello@gmail.com>
2013-10-01 11:45:41 -0300

    fix tests in ie

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,20 +15,39 @@ module.exports = function(grunt) {
       version: "10"
   }, {
       browserName: "internet explorer",
-      platform: "VISTA",
+      platform: "Windows 7",
       version: "9"
   }, {
       browserName: "internet explorer",
       platform: "XP",
       version: "8"
+  }, {
+      browserName: "safari",
+      platform: "OS X 10.8",
+      version: "6"
+  }, {
+      browserName: "android",
+      platform: "Linux",
+      version: "4.0"
+  }, {
+      browserName: "iphone",
+      platform: "OS X 10.8",
+      version: "6"
   }];
 
   grunt.initConfig({
     connect: {
-      server: {
+      test: {
         options: {
           base: "test",
+          hostname: '0.0.0.0',
           port: 9999
+        }
+      },
+      example: {
+        options: {
+          base: "example",
+          port: 3000
         }
       }
     },
@@ -45,6 +64,31 @@ module.exports = function(grunt) {
             }
         }
     },
+    browserify: {
+      dist: {
+        files: {
+          'build/auth0.js': ['index.js'],
+        }
+      }
+    },
+    uglify: {
+      min: {
+        files: {
+          'build/auth0.min.js': ['build/auth0.js']
+        }
+      }
+    },
+    copy: {
+      example: {
+        files: {
+          'example/auth0.js': 'build/auth0.js',
+          'test/auth0.js': 'build/auth0.js'
+        }
+      }
+    },
+    clean: {
+      build: ["build/", "example/auth0.js"],
+    },
     watch: {}
   });
 
@@ -53,6 +97,8 @@ module.exports = function(grunt) {
     if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
   }
 
-  grunt.registerTask("dev", ["connect", "watch"]);
-  grunt.registerTask("test", ["connect", "saucelabs-mocha"]);
+  grunt.registerTask("build",   ["clean", "browserify:dist", "uglify:min", "copy:example"]);
+  grunt.registerTask("example", ["connect:example", "watch"]);
+  grunt.registerTask("dev",     ["connect:test", "watch"]);
+  grunt.registerTask("test",    ["connect", "saucelabs-mocha"]);
 };
--- build/auth0.js
+++ build/auth0.js
@@ -329,6 +329,9 @@ var isRestorableProto = (function () {
 var indexOf = typeof Array.prototype.indexOf === 'function'
   ? function(arr, el) { return arr.indexOf(el); }
   : function(arr, el) {
+      if (typeof arr == 'string' && typeof "a"[0] == 'undefined') {
+        arr = arr.split('');
+      }
       for (var i = 0; i < arr.length; i++) {
         if (arr[i] === el) return i;
       }
