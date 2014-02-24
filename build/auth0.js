85b435aa6fcb154043bccaa278beb724180c8d2a
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2014-02-24 16:17:05 -0300

    login method: call loginWithUsernamePassword only if options.username/options.email is not undefined

--- build/auth0.js
+++ build/auth0.js
@@ -228,7 +228,8 @@ Auth0.prototype.changePassword = function (options, callback) {
 
 
 Auth0.prototype.login = Auth0.prototype.signin = function (options, callback) {
-  if (options.username || options.email) {
+  if (typeof options.username !== 'undefined' || 
+      typeof options.email !== 'undefined') {
     return this.loginWithUsernamePassword(options, callback);
   }
 
@@ -1195,12 +1196,13 @@ function decode(str) {
 }
 
 },{}],11:[function(require,module,exports){
-/*! version: 0.9.7
+/*! version: 0.9.1 */
+/*!
   * Reqwest! A general purpose XHR connection manager
-  * license MIT (c) Dustin Diaz 2013
+  * (c) Dustin Diaz 2013
   * https://github.com/ded/reqwest
+  * license MIT
   */
-
 !function (name, context, definition) {
   if (typeof module != 'undefined' && module.exports) module.exports = definition()
   else if (typeof define == 'function' && define.amd) define(definition)
@@ -1209,7 +1211,7 @@ function decode(str) {
 
   var win = window
     , doc = document
-    , twoHundo = /^(20\d|1223)$/
+    , twoHundo = /^20\d$/
     , byTag = 'getElementsByTagName'
     , readyState = 'readyState'
     , contentType = 'Content-Type'
@@ -1229,21 +1231,21 @@ function decode(str) {
           }
 
     , defaultHeaders = {
-          'contentType': 'application/x-www-form-urlencoded'
-        , 'requestedWith': xmlHttpRequest
-        , 'accept': {
+          contentType: 'application/x-www-form-urlencoded'
+        , requestedWith: xmlHttpRequest
+        , accept: {
               '*':  'text/javascript, text/html, application/xml, text/xml, */*'
-            , 'xml':  'application/xml, text/xml'
-            , 'html': 'text/html'
-            , 'text': 'text/plain'
-            , 'json': 'application/json, text/javascript'
-            , 'js':   'application/javascript, text/javascript'
+            , xml:  'application/xml, text/xml'
+            , html: 'text/html'
+            , text: 'text/plain'
+            , json: 'application/json, text/javascript'
+            , js:   'application/javascript, text/javascript'
           }
       }
 
     , xhr = function(o) {
         // is it x-domain
-        if (o['crossOrigin'] === true) {
+        if (o.crossOrigin === true) {
           var xhr = win[xmlHttpRequest] ? new XMLHttpRequest() : null
           if (xhr && 'withCredentials' in xhr) {
             return xhr
@@ -1280,23 +1282,23 @@ function decode(str) {
   }
 
   function setHeaders(http, o) {
-    var headers = o['headers'] || {}
+    var headers = o.headers || {}
       , h
 
-    headers['Accept'] = headers['Accept']
-      || defaultHeaders['accept'][o['type']]
-      || defaultHeaders['accept']['*']
+    headers.Accept = headers.Accept
+      || defaultHeaders.accept[o.type]
+      || defaultHeaders.accept['*']
 
     // breaks cross-origin requests with legacy browsers
-    if (!o['crossOrigin'] && !headers[requestedWith]) headers[requestedWith] = defaultHeaders['requestedWith']
-    if (!headers[contentType]) headers[contentType] = o['contentType'] || defaultHeaders['contentType']
+    if (!o.crossOrigin && !headers[requestedWith]) headers[requestedWith] = defaultHeaders.requestedWith
+    if (!headers[contentType]) headers[contentType] = o.contentType || defaultHeaders.contentType
     for (h in headers)
       headers.hasOwnProperty(h) && 'setRequestHeader' in http && http.setRequestHeader(h, headers[h])
   }
 
   function setCredentials(http, o) {
-    if (typeof o['withCredentials'] !== 'undefined' && typeof http.withCredentials !== 'undefined') {
-      http.withCredentials = !!o['withCredentials']
+    if (typeof o.withCredentials !== 'undefined' && typeof http.withCredentials !== 'undefined') {
+      http.withCredentials = !!o.withCredentials
     }
   }
 
@@ -1310,9 +1312,9 @@ function decode(str) {
 
   function handleJsonp(o, fn, err, url) {
     var reqId = uniqid++
-      , cbkey = o['jsonpCallback'] || 'callback' // the 'callback' key
-      , cbval = o['jsonpCallbackName'] || reqwest.getcallbackPrefix(reqId)
-      // , cbval = o['jsonpCallbackName'] || ('reqwest_' + reqId) // the 'callback' value
+      , cbkey = o.jsonpCallback || 'callback' // the 'callback' key
+      , cbval = o.jsonpCallbackName || reqwest.getcallbackPrefix(reqId)
+      // , cbval = o.jsonpCallbackName || ('reqwest_' + reqId) // the 'callback' value
       , cbreg = new RegExp('((^|\\?|&)' + cbkey + ')=([^&]+)')
       , match = url.match(cbreg)
       , script = doc.createElement('script')
@@ -1374,29 +1376,26 @@ function decode(str) {
 
   function getRequest(fn, err) {
     var o = this.o
-      , method = (o['method'] || 'GET').toUpperCase()
-      , url = typeof o === 'string' ? o : o['url']
-      // convert non-string objects to query-string form unless o['processData'] is false
-      , data = (o['processData'] !== false && o['data'] && typeof o['data'] !== 'string')
-        ? reqwest.toQueryString(o['data'])
-        : (o['data'] || null)
+      , method = (o.method || 'GET').toUpperCase()
+      , url = typeof o === 'string' ? o : o.url
+      // convert non-string objects to query-string form unless o.processData is false
+      , data = (o.processData !== false && o.data && typeof o.data !== 'string')
+        ? reqwest.toQueryString(o.data)
+        : (o.data || null)
       , http
       , sendWait = false
 
     // if we're working on a GET request and we have data then we should append
     // query string to end of URL and not post data
-    if ((o['type'] == 'jsonp' || method == 'GET') && data) {
+    if ((o.type == 'jsonp' || method == 'GET') && data) {
       url = urlappend(url, data)
       data = null
     }
 
-    if (o['type'] == 'jsonp') return handleJsonp(o, fn, err, url)
+    if (o.type == 'jsonp') return handleJsonp(o, fn, err, url)
 
-    // get the xhr from the factory if passed
-    // if the factory returns null, fall-back to ours
-    http = (o.xhr && o.xhr(o)) || xhr(o)
-
-    http.open(method, url, o['async'] === false ? false : true)
+    http = xhr(o)
+    http.open(method, url, o.async === false ? false : true)
     setHeaders(http, o)
     setCredentials(http, o)
     if (win[xDomainRequest] && http instanceof win[xDomainRequest]) {
@@ -1409,7 +1408,7 @@ function decode(str) {
     } else {
       http.onreadystatechange = handleReadyState(this, fn, err)
     }
-    o['before'] && o['before'](http)
+    o.before && o.before(http)
     if (sendWait) {
       setTimeout(function () {
         http.send(data)
@@ -1434,7 +1433,7 @@ function decode(str) {
 
   function init(o, fn) {
 
-    this.url = typeof o == 'string' ? o : o['url']
+    this.url = typeof o == 'string' ? o : o.url
     this.timeout = null
 
     // whether request has been fulfilled for purpose
@@ -1451,36 +1450,36 @@ function decode(str) {
     this._responseArgs = {}
 
     var self = this
-      , type = o['type'] || setType(this.url)
+      , type = o.type || setType(this.url)
 
     fn = fn || function () {}
 
-    if (o['timeout']) {
+    if (o.timeout) {
       this.timeout = setTimeout(function () {
         self.abort()
-      }, o['timeout'])
+      }, o.timeout)
     }
 
-    if (o['success']) {
+    if (o.success) {
       this._successHandler = function () {
-        o['success'].apply(o, arguments)
+        o.success.apply(o, arguments)
       }
     }
 
-    if (o['error']) {
+    if (o.error) {
       this._errorHandlers.push(function () {
-        o['error'].apply(o, arguments)
+        o.error.apply(o, arguments)
       })
     }
 
-    if (o['complete']) {
+    if (o.complete) {
       this._completeHandlers.push(function () {
-        o['complete'].apply(o, arguments)
+        o.complete.apply(o, arguments)
       })
     }
 
     function complete (resp) {
-      o['timeout'] && clearTimeout(self.timeout)
+      o.timeout && clearTimeout(self.timeout)
       self.timeout = null
       while (self._completeHandlers.length > 0) {
         self._completeHandlers.shift()(resp)
@@ -1621,8 +1620,8 @@ function decode(str) {
       , optCb = function (o) {
           // IE gives value="" even where there is no value attribute
           // 'specified' ref: http://www.w3.org/TR/DOM-Level-3-Core/core.html#ID-862529273
-          if (o && !o['disabled'])
-            cb(n, normalize(o['attributes']['value'] && o['attributes']['value']['specified'] ? o['value'] : o['text']))
+          if (o && !o.disabled)
+            cb(n, normalize(o.attributes.value && o.attributes.value.specified ? o.value : o.text))
         }
       , ch, ra, val, i
 
@@ -1729,12 +1728,12 @@ function decode(str) {
         }
     // If an array was passed in, assume that it is an array of form elements.
     if (isArray(o)) {
-      for (i = 0; o && i < o.length; i++) add(o[i]['name'], o[i]['value'])
+      for (i = 0; o && i < o.length; i++) add(o[i].name, o[i].value)
     } else {
       // If traditional, encode the "old" way (the way 1.3.2 or older
       // did it), otherwise encode params recursively.
       for (prefix in o) {
-        if (o.hasOwnProperty(prefix)) buildParams(prefix, o[prefix], traditional, add)
+        buildParams(prefix, o[prefix], traditional, add)
       }
     }
 
@@ -1777,10 +1776,10 @@ function decode(str) {
   // .ajax.compat(options, callback)
   reqwest.compat = function (o, fn) {
     if (o) {
-      o['type'] && (o['method'] = o['type']) && delete o['type']
-      o['dataType'] && (o['type'] = o['dataType'])
-      o['jsonpCallback'] && (o['jsonpCallbackName'] = o['jsonpCallback']) && delete o['jsonpCallback']
-      o['jsonp'] && (o['jsonpCallback'] = o['jsonp'])
+      o.type && (o.method = o.type) && delete o.type
+      o.dataType && (o.type = o.dataType)
+      o.jsonpCallback && (o.jsonpCallbackName = o.jsonpCallback) && delete o.jsonpCallback
+      o.jsonp && (o.jsonpCallback = o.jsonp)
     }
     return new Reqwest(o, fn)
   }
