5750120b6c32a7e90466da23b32917808a5279a0
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-08-12 12:06:16 -0300

    Add test for most common parseHash scenarios

--- test/tests.js
+++ test/tests.js
@@ -310,23 +310,321 @@ describe('Auth0', function () {
     })
   });
 
-  describe('parseHash', function () {
-
-    it('should be able to parse the profile', function () {
-      var hash = '#access_token=jFxsZUQTJXXwcwIm&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2xvZ2luLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExODMwNDIzMTY0MDMwMTY4NTU3OSIsImF1ZCI6IjBIUDcxR1NkNlB1b1JZSjNEWEtkaVhDVVVkR21CYnVwIiwiZXhwIjoxMzgwMjU4NzU4LCJpYXQiOjEzODAyMjI3NTgsImNsaWVudElEIjoiMEhQNzFHU2Q2UHVvUllKM0RYS2RpWENVVWRHbUJidXAiLCJlbWFpbCI6Impvc2Uucm9tYW5pZWxsb0BxcmFmdGxhYnMuY29tIiwiZmFtaWx5X25hbWUiOiJSb21hbmllbGxvIiwiZ2VuZGVyIjoibWFsZSIsImdpdmVuX25hbWUiOiJKb3NlIiwiaWRlbnRpdGllcyI6W3siYWNjZXNzX3Rva2VuIjoieWEyOS5BSEVTNlpUSllmQnN3a1NFbUU2YTQ2SlpHYVgxV1Jqc2ZrUzd5Vm81RXNPdktKWVhnenpEZl9ZUiIsInByb3ZpZGVyIjoiZ29vZ2xlLW9hdXRoMiIsInVzZXJfaWQiOiIxMTgzMDQyMzE2NDAzMDE2ODU1NzkiLCJjb25uZWN0aW9uIjoiZ29vZ2xlLW9hdXRoMiIsImlzU29jaWFsIjp0cnVlfV0sImxvY2FsZSI6ImVuIiwibmFtZSI6Ikpvc2UgUm9tYW5pZWxsbyIsIm5pY2tuYW1lIjoiam9zZS5yb21hbmllbGxvIiwicGljdHVyZSI6Imh0dHBzOi8vbGg2Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8tcF81dUwxTDFkdkUvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQlEvaVBIRUQ0ajlxblkvcGhvdG8uanBnIiwidXNlcl9pZCI6Imdvb2dsZS1vYXV0aDJ8MTE4MzA0MjMxNjQwMzAxNjg1NTc5In0.Qrhrkp7hCYFyN_Ax9yVPKztuJNFHjnGbyUfLJsccLGU&token_type=bearer&state=Ttct3tBlHDhRnXCv&refresh_token=gonto';
-
-      var auth0 = new Auth0({
-        clientID:     '0HP71GSd6PuoRYJ3DXKdiXCUUdGmBbup',
-        callbackURL:  'https://myapp.com/callback',
-        domain:       'login.auth0.com'
+  describe.only('parseHash', function () {
+    context('response_type=token + scope=openid offline_access + state', function() {
+      before(function() {
+        var hash = '#access_token=AdyWpLVbQi2GA0fy&refresh_token=8m8M2Dk7BWsmpyumpguR4ZVKpZDy6bhFrZacaq6kmEVtt&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTYzOTQyLCJpYXQiOjE0NzA5Mjc5NDJ9.KcxIWhnTHeL_kNwUq74ef3REOCFDxiOH_NiNMqNNZks&token_type=Bearer&state=hello';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
       });
 
-      var result = auth0.parseHash(hash);
-      expect(result.idTokenPayload.name).to.eql('Jose Romaniello');
-      expect(result.accessToken).to.eql('jFxsZUQTJXXwcwIm');
-      expect(result.refreshToken).to.eql('gonto');
-      expect(result.state).to.eql('Ttct3tBlHDhRnXCv');
+      it('copies the access_token', function() {
+        expect(this.parsedHash.accessToken).to.be('AdyWpLVbQi2GA0fy');
+      });
 
+      it('copies the id_token', function() {
+        expect(this.parsedHash.idToken).to.be('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTYzOTQyLCJpYXQiOjE0NzA5Mjc5NDJ9.KcxIWhnTHeL_kNwUq74ef3REOCFDxiOH_NiNMqNNZks');
+      });
+
+      it('decodes the id_token', function() {
+        expect(this.parsedHash.idTokenPayload.aud).to.be('K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0');
+      });
+
+      it('copies the refresh_token', function() {
+        expect(this.parsedHash.refreshToken).to.be('8m8M2Dk7BWsmpyumpguR4ZVKpZDy6bhFrZacaq6kmEVtt');
+      });
+
+      it('copies the sate', function() {
+        expect(this.parsedHash.state).to.be('hello');
+      });
+
+      it('doesn\'t have an error', function() {
+        expect(this.parsedHash.error).to.be(undefined);
+      });
+    });
+
+    context('response_type=token + scope=openid offline_access', function() {
+      before(function() {
+        var hash = '#access_token=meZc5MnnwwL0LyZO&refresh_token=Xqs1iD2F4IxL3C9WaOaDllZd5ns411967JPPZubuf8K8H&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0MDU2LCJpYXQiOjE0NzA5MjgwNTZ9.zM12OViHQQkSogcW_-CXat_2cOMIHy0JShbbNIxKRkM&token_type=Bearer';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('copies the access_token', function() {
+        expect(this.parsedHash.accessToken).to.be('meZc5MnnwwL0LyZO');
+      });
+
+      it('copies the id_token', function() {
+        expect(this.parsedHash.idToken).to.be('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0MDU2LCJpYXQiOjE0NzA5MjgwNTZ9.zM12OViHQQkSogcW_-CXat_2cOMIHy0JShbbNIxKRkM');
+      });
+
+      it('decodes the id_token', function() {
+        expect(this.parsedHash.idTokenPayload.aud).to.be('K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0');
+      });
+
+      it('copies the refresh_token', function() {
+        expect(this.parsedHash.refreshToken).to.be('Xqs1iD2F4IxL3C9WaOaDllZd5ns411967JPPZubuf8K8H');
+      });
+
+      it('doesn\'t include sate', function() {
+        expect(this.parsedHash.state).to.be(undefined);
+      });
+
+      it('doesn\'t have an error', function() {
+        expect(this.parsedHash.error).to.be(undefined);
+      });
+    });
+
+    context('response_type=token + scope=openid + state', function() {
+      before(function() {
+        var hash = '#access_token=I6MceMUVoKxyWhJN&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0MTIwLCJpYXQiOjE0NzA5MjgxMjB9.tkUFnd9oi5AAo9yraQwkrn5Z1D-G4HX3wzQ1yWSM81g&token_type=Bearer&state=hello';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('copies the access_token', function() {
+        expect(this.parsedHash.accessToken).to.be('I6MceMUVoKxyWhJN');
+      });
+
+      it('copies the id_token', function() {
+        expect(this.parsedHash.idToken).to.be('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0MTIwLCJpYXQiOjE0NzA5MjgxMjB9.tkUFnd9oi5AAo9yraQwkrn5Z1D-G4HX3wzQ1yWSM81g');
+      });
+
+      it('decodes the id_token', function() {
+        expect(this.parsedHash.idTokenPayload.aud).to.be('K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0');
+      });
+
+      it('doesn\'t include a refresh_token', function() {
+        expect(this.parsedHash.refreshToken).to.be(undefined);
+      });
+
+      it('copies the sate', function() {
+        expect(this.parsedHash.state).to.be('hello');
+      });
+
+      it('doesn\'t have an error', function() {
+        expect(this.parsedHash.error).to.be(undefined);
+      });
+    });
+
+    context('response_type=token + scope=openid', function() {
+      before(function() {
+        var hash = '#access_token=kb1t8RwAmevjnV2F&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0MTY5LCJpYXQiOjE0NzA5MjgxNjl9.KC6stFcLPFnEPMmRfRVoM3Fe2WMNLBn68Aa63kyZ5gI&token_type=Bearer';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('copies the access_token', function() {
+        expect(this.parsedHash.accessToken).to.be('kb1t8RwAmevjnV2F');
+      });
+
+      it('copies the id_token', function() {
+        expect(this.parsedHash.idToken).to.be('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0MTY5LCJpYXQiOjE0NzA5MjgxNjl9.KC6stFcLPFnEPMmRfRVoM3Fe2WMNLBn68Aa63kyZ5gI');
+      });
+
+      it('decodes the id_token', function() {
+        expect(this.parsedHash.idTokenPayload.aud).to.be('K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0');
+      });
+
+      it('doesn\'t include a refresh_token', function() {
+        expect(this.parsedHash.refreshToken).to.be(undefined);
+      });
+
+      it('doesn\'t include state', function() {
+        expect(this.parsedHash.state).to.be(undefined);
+      });
+
+      it('doesn\'t have an error', function() {
+        expect(this.parsedHash.error).to.be(undefined);
+      });
+    });
+
+    context('response_type=token + state', function() {
+      before(function() {
+        var hash = '#access_token=thu2az95NNmhCfeZ&token_type=Bearer&state=hello';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('copies the access_token', function() {
+        expect(this.parsedHash.accessToken).to.be('thu2az95NNmhCfeZ');
+      });
+
+      it('doesn\'t include an id_token', function() {
+        expect(this.parsedHash.idToken).to.be(undefined);
+      });
+
+      it('doesn\'t decode an id_token', function() {
+        expect(this.parsedHash.idTokenPayload).to.be(undefined);
+      });
+
+      it('doesn\'t include a refresh_token', function() {
+        expect(this.parsedHash.refreshToken).to.be(undefined);
+      });
+
+      it('copies the state', function() {
+        expect(this.parsedHash.state).to.be('hello');
+      });
+
+      it('doesn\'t have an error', function() {
+        expect(this.parsedHash.error).to.be(undefined);
+      });
+    });
+
+    context('response_type=token', function() {
+      before(function() {
+        var hash = '#access_token=cpiUDP1E8zX1Dfyw&token_type=Bearer';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('copies the access_token', function() {
+        expect(this.parsedHash.accessToken).to.be('cpiUDP1E8zX1Dfyw');
+      });
+
+      it('doesn\'t include an id_token', function() {
+        expect(this.parsedHash.idToken).to.be(undefined);
+      });
+
+      it('doesn\'t decode an id_token', function() {
+        expect(this.parsedHash.idTokenPayload).to.be(undefined);
+      });
+
+      it('doesn\'t include a refresh_token', function() {
+        expect(this.parsedHash.refreshToken).to.be(undefined);
+      });
+
+      it('doesn\'t include state', function() {
+        expect(this.parsedHash.state).to.be(undefined);
+      });
+
+      it('doesn\'t have an error', function() {
+        expect(this.parsedHash.error).to.be(undefined);
+      });
+    });
+
+    context('response_type=id_token + scope=openid + state', function() {
+      before(function() {
+        var hash = '#id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0NzE0LCJpYXQiOjE0NzA5Mjg3MTR9.mQ-OLmCuoveYeH3PhDBXYJOwq8sSfdOieXzUoZqZT2k&state=hello';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('doesn\'t include an access_token', function() {
+        expect(this.parsedHash.accessToken).to.be(undefined);
+      });
+
+      it('copies the id_token', function() {
+        expect(this.parsedHash.idToken).to.be('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0NzE0LCJpYXQiOjE0NzA5Mjg3MTR9.mQ-OLmCuoveYeH3PhDBXYJOwq8sSfdOieXzUoZqZT2k');
+      });
+
+      it('decodes the id_token', function() {
+        expect(this.parsedHash.idTokenPayload.aud).to.be('K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0');
+      });
+
+      it('doesn\'t include a refresh_token', function() {
+        expect(this.parsedHash.refreshToken).to.be(undefined);
+      });
+
+      it('copies the state', function() {
+        expect(this.parsedHash.state).to.be('hello');
+      });
+
+      it('doesn\'t have an error', function() {
+        expect(this.parsedHash.error).to.be(undefined);
+      });
+    });
+
+    context('response_type=id_token', function() {
+      before(function() {
+        var hash = '#id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0NzU0LCJpYXQiOjE0NzA5Mjg3NTR9.gsjJQyYJzIShiBcI02i4fsGk68nbSCOLojReI2czI7Y';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('doesn\'t include an access_token', function() {
+        expect(this.parsedHash.accessToken).to.be(undefined);
+      });
+
+      it('copies the id_token', function() {
+        expect(this.parsedHash.idToken).to.be('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2duYW5kcmV0dGEuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU3MmNhOWYzMGRjMjhkOGQ3YmY3MzRhYSIsImF1ZCI6Iks2bkFFT2dFZVN3b2dDR3Y2TjZtOXdOZlFodmJGQW0wIiwiZXhwIjoxNDcwOTY0NzU0LCJpYXQiOjE0NzA5Mjg3NTR9.gsjJQyYJzIShiBcI02i4fsGk68nbSCOLojReI2czI7Y');
+      });
+
+      it('decodes the id_token', function() {
+        expect(this.parsedHash.idTokenPayload.aud).to.be('K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0');
+      });
+
+      it('doesn\'t include a refresh_token', function() {
+        expect(this.parsedHash.refreshToken).to.be(undefined);
+      });
+
+      it('doesn\'t include state', function() {
+        expect(this.parsedHash.state).to.be(undefined);
+      });
+
+      it('doesn\'t have an error', function() {
+        expect(this.parsedHash.error).to.be(undefined);
+      });
+    });
+
+    context("error + state", function() {
+      before(function() {
+        var hash = '#error=unauthorized&error_description=My%20custom%20error%20message&state=hello';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('copies the error', function() {
+        expect(this.parsedHash.error).to.be('unauthorized');
+      });
+
+      it('copies the error message', function() {
+        expect(this.parsedHash.error_description).to.be('My custom error message');
+      });
+
+      it('copies the state', function() {
+        expect(this.parsedHash.state).to.be('hello');
+      });
+    });
+
+    context("error", function() {
+      before(function() {
+        var hash = '#error=unauthorized&error_description=My%20custom%20error%20message';
+        this.parsedHash = new Auth0({
+          clientID: 'K6nAEOgEeSwogCGv6N6m9wNfQhvbFAm0',
+          domain: 'gnandretta.auth0.com'
+        }).parseHash(hash);
+      });
+
+      it('copies the error', function() {
+        expect(this.parsedHash.error).to.be('unauthorized');
+      });
+
+      it('copies the error message', function() {
+        expect(this.parsedHash.error_description).to.be('My custom error message');
+      });
+
+      it('doesn\'t include state', function() {
+        expect(this.parsedHash.state).to.be(undefined);
+      });
     });
 
     it('should be able to parse the profile (if it starts with a slash)', function () {
@@ -400,21 +698,6 @@ describe('Auth0', function () {
       var result = auth0.parseHash(hash);
       expect(result.error).to.be.equal('invalid_token');
       expect(result.error_description).to.be.equal('The clientID configured (wrong) does not match with the clientID set in the token (0HP71GSd6PuoRYJ3DXKdiXCUUdGmBbup, 1JQ82GSd6PuoRYJ3DXKdiXCUUdGmBbup).');
-    });
-
-    it('should be able to parse an error', function () {
-      var hash = '#error=invalid_grant&error_description=this%20is%20a%20cool%20error%20description';
-
-      var auth0 = new Auth0({
-        clientID:     'aaaabcdefgh',
-        callbackURL:  'https://myapp.com/callback',
-        domain:       'aaa.auth0.com'
-      });
-
-      var result = auth0.parseHash(hash);
-      expect(result.error).to.be.equal('invalid_grant');
-      expect(result.error_description).to.be.equal('this is a cool error description');
-
     });
 
     it('should be able to parse an error (if it starts with a slash)', function () {
