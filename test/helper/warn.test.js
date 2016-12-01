9144a6b94e243ac019d8056028d57d172d966a30
German Lena <german.lena@gmail.com>
2016-12-01 12:25:50 -0300

    test for warn

---
+++ test/helper/warn.test.js
@@ -0,0 +1,34 @@
+var expect = require('expect.js');
+
+var stub = require('sinon').stub;
+
+var Warn = require('../../src/helper/warn');
+
+describe('helpers warn', function () {
+
+  afterEach(function(){
+    console.warn.restore();
+  })
+
+  it('should show a warning in the console', function () {
+
+    stub(console, 'warn', function (message) {
+      expect(message).to.be('the message')
+    });
+
+    var warn = new Warn({});
+    warn.warning('the message');
+
+  });
+
+  it('should not show a warning in the console', function () {
+
+    stub(console, 'warn', function (message) {
+      throw Error('warn was called')
+    });
+
+    var warn = new Warn({ disableWarnings: true });
+    warn.warning('the message');
+  });
+
+});
