9a4fafc0ad9fedc2ba2ea5d339b4364ae722700f
German Lena <german.lena@gmail.com>
2016-12-05 10:22:22 -0300

    export version in the bundle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 var Authentication = require('./authentication');
 var Management = require('./management');
 var WebAuth = require('./web-auth');
+var version = require('./version');
 
 module.exports = {
   Authentication: Authentication,
   Management: Management,
-  WebAuth: WebAuth
+  WebAuth: WebAuth,
+  version: version.raw
 };
