a6c074ebcd43f6b9c9f9fc5fdc66196c96fe08c1
German Lena <german.lena@gmail.com>
2016-12-07 11:30:01 -0300

    fix test

--- test/web-auth/web-auth.test.js
+++ test/web-auth/web-auth.test.js
@@ -224,7 +224,7 @@ describe('auth0.WebAuth', function () {
 
     it('should pass the correct authorize url', function (done) {
       stub(IframeHandler.prototype, 'init', function(message) {
-        expect(this.url).to.be('https://me.auth0.com/authorize?client_id=...&response_type=id_token&redirect_uri=http%3A%2F%2Fpage.com%2Fcallback&audience=urn%3Asite%3Ademo%3Ablog&scope=openid%20name%20read%3Ablog&state=456&nonce=123&prompt=none');
+        expect(this.url).to.be('https://me.auth0.com/authorize?client_id=...&response_type=id_token&response_mode=fragment&redirect_uri=http%3A%2F%2Fpage.com%2Fcallback&audience=urn%3Asite%3Ademo%3Ablog&scope=openid%20name%20read%3Ablog&state=456&nonce=123&prompt=none');
         this.timeoutCallback();
       });
 
