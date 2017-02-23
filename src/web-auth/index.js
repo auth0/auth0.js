01c4b7b0f3f3456e41e48f5c2e1d05a24611be7d
Hernan Zalazar <hernan.zalazar@gmail.com>
2017-02-23 19:15:01 -0300

    Fix broken tests

--- src/web-auth/index.js
+++ src/web-auth/index.js
@@ -78,6 +78,7 @@ function WebAuth(options) {
  *
  * @method parseHash
  * @param {Object} options:
+ * @param {String} options._idTokenVerification [OPTIONAL] Default: true
  * @param {String} options.state [OPTIONAL] to verify the response
  * @param {String} options.nonce [OPTIONAL] to verify the id_token
  * @param {String} options.hash [OPTIONAL] the url hash. If not provided it will extract from window.location.hash
@@ -97,6 +98,8 @@ WebAuth.prototype.parseHash = function (options, cb) {
   } else {
     options = options || {};
   }
+
+  options._idTokenVerification = !(options._idTokenVerification === false);
 
   var _window = windowHelper.getWindow();
 
