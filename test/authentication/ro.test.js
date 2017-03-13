6ede6461a323278a6b8a72bf0fca3fd04d30e607
Hernan Zalazar <hzalaz@users.noreply.github.com>
2017-03-13 11:42:06 -0300

    Whitelist resource owner parameters (#386)

--- test/authentication/ro.test.js
+++ test/authentication/ro.test.js
@@ -115,7 +115,7 @@ describe('auth0.authentication', function () {
         expect(url).to.be('https://me.auth0.com/oauth/ro');
         return new RequestMock({
           body: {
-            client_id: '123',
+            client_id: '...',
             grant_type: 'password',
             username: 'the username',
             password: 'the password',
@@ -139,11 +139,53 @@ describe('auth0.authentication', function () {
       });
 
       this.auth0.loginWithResourceOwner({
-        clientID: '123',
         username: 'the username',
         password: 'the password',
         connection: 'the_connection',
         scope: 'openid'
+      }, function (err, data) {
+        expect(err).to.be(null);
+        expect(data).to.eql({
+          idToken: 'id_token.id_token.id_token',
+          accessToken: 'access_token',
+          tokenType: 'bearer'
+        });
+        done();
+      });
+    });
+
+    it('should exclude parameters not in whitelist', function (done) {
+      stub(request, 'post', function (url) {
+        expect(url).to.be('https://me.auth0.com/oauth/ro');
+        return new RequestMock({
+          body: {
+            client_id: '...',
+            grant_type: 'password',
+            username: 'the username',
+            password: 'the password',
+            connection: 'the_connection'
+          },
+          headers: {
+            'Content-Type': 'application/json',
+            'Auth0-Client': telemetryInfo
+          },
+          cb: function (cb) {
+            cb(null, {
+              body: {
+                id_token: 'id_token.id_token.id_token',
+                access_token: 'access_token',
+                token_type: 'bearer'
+              }
+            });
+          }
+        });
+      });
+
+      this.auth0.loginWithResourceOwner({
+        username: 'the username',
+        password: 'the password',
+        connection: 'the_connection',
+        should_ignore: { invalid: 'invalid value' }
       }, function (err, data) {
         expect(err).to.be(null);
         expect(data).to.eql({
