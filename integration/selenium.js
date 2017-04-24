3934200d3e04608bca01a4ac7668e485f1ef1f72
Hernan Zalazar <hernan.zalazar@gmail.com>
2017-04-23 21:13:08 -0300

    Add more working browsers.
    IE are commented since css selector always fails

--- integration/selenium.js
+++ integration/selenium.js
@@ -8,11 +8,48 @@ var capabilities = [
   'browserName': 'chrome',
   'platform': 'Windows 10',
   'version': '55.0'
-},
+}
+,
 {
   'browserName': 'chrome',
   'platform': 'Windows 10',
   'version': 'beta'
+}
+,
+{
+  'browserName': 'firefox',
+  'platform': 'Windows 10',
+  'version': '50.0'
+}
+// ,
+// {
+//   'browserName': 'firefox',
+//   'platform': 'Windows 10',
+//   'version': 'beta'
+// }
+// ,
+// {
+//   'browserName': 'internet explorer',
+//   'platform': 'Windows 7',
+//   'version': '11.0'
+// }
+// ,
+// {
+//   'browserName': 'internet explorer',
+//   'platform': 'Windows 7',
+//   'version': '10.0'
+// }
+// ,
+// {
+//   'browserName': 'MicrosoftEdge',
+//   'platform': 'Windows 10',
+//   'version': '13.10586'
+// }
+// ,
+{
+  'browserName': 'safari',
+  'platform': 'macOS 10.12',
+  'version': '10.0'
 }
 ];
 
