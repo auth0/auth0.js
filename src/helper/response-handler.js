c569abcc8c6a244eb72f2e66b4dae1c6973ab33d
Luis Deschamps Rudge <luis@luisrudge.net>
2018-03-02 15:57:35 -0300

    assigning the legacy error values after normalizing them

--- src/helper/response-handler.js
+++ src/helper/response-handler.js
@@ -42,10 +42,6 @@ function wrapCallback(cb, options) {
       if (err.err) {
         err = err.err;
       }
-      if (options.forceLegacyError) {
-        errObj.error = err.error;
-        errObj.error_description = err.error_description;
-      }
       errObj.code = err.error || err.code || err.error_code || err.status || null;
       errObj.description =
         err.errorDescription ||
@@ -55,6 +51,10 @@ function wrapCallback(cb, options) {
         err.details ||
         err.err ||
         null;
+      if (options.forceLegacyError) {
+        errObj.error = errObj.code;
+        errObj.error_description = errObj.description;
+      }
 
       if (err.name) {
         errObj.name = err.name;
