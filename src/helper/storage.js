auth0/auth0.js

commit dcf71380ce780490d34d12dcdffed7e858aa11f9

Fix storing values when DOM storage is not available (#737)
* add js-cookie to handle Cookies

* Simplifying storage helper and writting better tests

* Writting better tests for storage handler

* Using storage helper instead of sessionStorage directly

* adding `expires` to transactionManager data

* decoupling webauth tests from storage/localstorage

* adding telemetry tests

* adding expect to dummy storage

* add tests for base64_url

* fix base64_url test

* Using constants for time intervals

* fix null/undefined error

* adding missing tests for localStorage and cookies

src/helper/storage.js
@@ -1,26 +1,20 @@
 var StorageHandler = require('./storage/handler');
-var storage;
-
-function getStorage(force) {
-  if (!storage || force) {
-    storage = new StorageHandler();
-  }
-  return storage;
-}
+var storage = new StorageHandler();
 
 module.exports = {
   getItem: function(key) {
-    var value = getStorage().getItem(key);
-    return value ? JSON.parse(value) : value;
+    var value = storage.getItem(key);
+    try {
+      return JSON.parse(value);
+    } catch (_) {
+      return value;
+    }
   },
   removeItem: function(key) {
-    return getStorage().removeItem(key);
+    return storage.removeItem(key);
   },
-  setItem: function(key, value) {
+  setItem: function(key, value, options) {
     var json = JSON.stringify(value);
-    return getStorage().setItem(key, json);
-  },
-  reload: function() {
-    getStorage(true);
+    return storage.setItem(key, json, options);
   }
 };

src/helper/cookies.js
@@ -1,63 +1,0 @@
-var windowHandler = require('./window');
-var base64Url = require('./base64_url');
-
-function create(name, value, days) {
-  var date;
-  var expires;
-
-  if (
-    windowHandler.getDocument().cookie === undefined ||
-    windowHandler.getDocument().cookie === null
-  ) {
-    throw new Error('cookie storage not available');
-  }
-
-  if (days) {
-    var timeToExpire = days * 24 * 60 * 60 * 1000;
-    date = new Date();
-    date.setTime(date.getTime() + timeToExpire);
-    expires = '; expires=' + date.toGMTString();
-  } else {
-    expires = '';
-  }
-
-  windowHandler.getDocument().cookie = name + '=' + base64Url.encode(value) + expires + '; path=/';
-}
-
-function read(name) {
-  var i;
-  var cookie;
-  var cookies;
-  var nameEQ = name + '=';
-
-  if (
-    windowHandler.getDocument().cookie === undefined ||
-    windowHandler.getDocument().cookie === null
-  ) {
-    throw new Error('cookie storage not available');
-  }
-
-  cookies = windowHandler.getDocument().cookie.split(';');
-
-  for (i = 0; i < cookies.length; i++) {
-    cookie = cookies[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1, cookie.length);
-    }
-    if (cookie.indexOf(nameEQ) === 0) {
-      return base64Url.decode(cookie.substring(nameEQ.length, cookie.length));
-    }
-  }
-
-  return null;
-}
-
-function erase(name) {
-  create(name, '', -1);
-}
-
-module.exports = {
-  create: create,
-  read: read,
-  erase: erase
-};

src/helper/storage/cookie.js
@@ -1,17 +1,23 @@
-var cookies = require('../cookies');
-
+var Cookie = require('js-cookie');
+var objectHelper = require('../object');
 function CookieStorage() {}
 
 CookieStorage.prototype.getItem = function(key) {
-  return cookies.read(key);
+  return Cookie.get(key);
 };
 
 CookieStorage.prototype.removeItem = function(key) {
-  cookies.erase(key);
+  Cookie.remove(key);
 };
 
-CookieStorage.prototype.setItem = function(key, value) {
-  cookies.create(key, value, 1);
+CookieStorage.prototype.setItem = function(key, value, options) {
+  var params = objectHelper.extend(
+    {
+      expires: 1 // 1 day
+    },
+    options
+  );
+  Cookie.set(key, value, params);
 };
 
 module.exports = CookieStorage;

src/helper/storage/handler.js
@@ -9,7 +9,10 @@ function StorageHandler() {
   try {
     // some browsers throw an error when trying to access localStorage
     // when localStorage is disabled.
-    this.storage = windowHandler.getWindow().localStorage;
+    var localStorage = windowHandler.getWindow().localStorage;
+    if (localStorage) {
+      this.storage = localStorage;
+    }
   } catch (e) {
     this.warn.warning(e);
     this.warn.warning("Can't use localStorage. Using CookieStorage instead.");
@@ -49,13 +52,13 @@ StorageHandler.prototype.removeItem = function(key) {
   }
 };
 
-StorageHandler.prototype.setItem = function(key, value) {
+StorageHandler.prototype.setItem = function(key, value, options) {
   try {
-    return this.storage.setItem(key, value);
+    return this.storage.setItem(key, value, options);
   } catch (e) {
     this.warn.warning(e);
     this.failover();
-    return this.setItem(key, value);
+    return this.setItem(key, value, options);
   }
 };
 

src/helper/times.js
@@ -1,0 +1,2 @@
+module.exports.MINUTES_15 = 1 / 96;
+module.exports.MINUTES_30 = 1 / 48;

src/web-auth/cross-origin-authentication.js
@@ -5,6 +5,8 @@ var objectHelper = require('../helper/object');
 var RequestBuilder = require('../helper/request-builder');
 var WebMessageHandler = require('./web-message-handler');
 var responseHandler = require('../helper/response-handler');
+var storage = require('../helper/storage');
+var times = require('../helper/times');
 
 function CrossOriginAuthentication(webAuth, options) {
   this.webAuth = webAuth;
@@ -41,7 +43,6 @@ function createKey(origin, coId) {
  */
 CrossOriginAuthentication.prototype.login = function(options, cb) {
   var _this = this;
-  var theWindow = windowHelper.getWindow();
   var url = urljoin(this.baseOptions.rootUrl, '/co/authenticate');
   options.username = options.username || options.email;
   delete options.email;
@@ -82,7 +83,7 @@ CrossOriginAuthentication.prototype.login = function(options, cb) {
       .merge(options)
       .with({ loginTicket: data.body.login_ticket });
     var key = createKey(_this.baseOptions.rootUrl, data.body.co_id);
-    theWindow.sessionStorage[key] = data.body.co_verifier;
+    storage.setItem(key, data.body.co_verifier, { expires: times.MINUTES_15 });
     if (popupMode) {
       _this.webMessageHandler.run(
         authorizeOptions,
@@ -96,9 +97,9 @@ CrossOriginAuthentication.prototype.login = function(options, cb) {
 
 function tryGetVerifier(theWindow, key) {
   try {
-    var verifier = theWindow.sessionStorage[key];
-    theWindow.sessionStorage.removeItem(key);
-    return verifier;
+    var verifier = storage.getItem(key);
+    storage.removeItem(key);
+    return verifier || '';
   } catch (e) {
     return '';
   }

src/web-auth/transaction-manager.js
@@ -1,5 +1,6 @@
 var random = require('../helper/random');
 var storage = require('../helper/storage');
+var times = require('../helper/times');
 
 var DEFAULT_NAMESPACE = 'com.auth0.auth.';
 
@@ -44,12 +45,16 @@ TransactionManager.prototype.generateTransaction = function(
   state = state || random.randomString(this.keyLength);
   nonce = nonce || (generateNonce ? random.randomString(this.keyLength) : null);
 
-  storage.setItem(this.namespace + state, {
-    nonce: nonce,
-    appState: appState,
-    state: state,
-    lastUsedConnection: lastUsedConnection
-  });
+  storage.setItem(
+    this.namespace + state,
+    {
+      nonce: nonce,
+      appState: appState,
+      state: state,
+      lastUsedConnection: lastUsedConnection
+    },
+    times.MINUTES_30
+  );
   return {
     state: state,
     nonce: nonce

test/helper/base64_url.test.js
@@ -1,0 +1,28 @@
+var expect = require('expect.js');
+var base64url = require('../../src/helper/base64_url');
+
+var tests = [
+  { decoded: 'test', encoded: 'dGVzdA==' },
+  { decoded: JSON.stringify({ foo: 'bar' }), encoded: 'eyJmb28iOiJiYXIifQ==' },
+  { decoded: 'a', encoded: 'YQ==' }
+];
+
+describe('helpers base64_url', function() {
+  describe('encode', function() {
+    tests.forEach(function(s) {
+      it('should encode ' + s.decoded + ' to ' + s.encoded, function() {
+        expect(base64url.encode(s.decoded)).to.be(s.encoded);
+      });
+    });
+  });
+  describe('decode', function() {
+    tests.forEach(function(s) {
+      it('should decode ' + s.decoded, function() {
+        expect(base64url.decode(s.encoded)).to.be(s.decoded);
+      });
+    });
+    it('should handle special case when `str.length % 4 !== 0`', () => {
+      expect(base64url.decode('YW=')).to.be('a');
+    });
+  });
+});

commit 666fc64f4d7739d4955a660d0ae08b10a0de0429

restore lazy instantiation of StorageHandler()
to avoid instantiating the StorageHandler, which requires the global window object, upon module load

bug was introduced in https://github.com/auth0/auth0.js/pull/737

addresses issue: https://github.com/auth0/auth0.js/issues/744

src/helper/storage.js
@@ -1,9 +1,16 @@
 var StorageHandler = require('./storage/handler');
-var storage = new StorageHandler();
+var storage = false;
+
+function getStorage(force) {
+  if (!storage || force) {
+    storage = new StorageHandler();
+  }
+  return storage;
+}
 
 module.exports = {
   getItem: function(key) {
-    var value = storage.getItem(key);
+    var value = getStorage().getItem(key);
     try {
       return JSON.parse(value);
     } catch (_) {
@@ -11,10 +18,13 @@ module.exports = {
     }
   },
   removeItem: function(key) {
-    return storage.removeItem(key);
+    return getStorage().removeItem(key);
   },
   setItem: function(key, value, options) {
     var json = JSON.stringify(value);
-    return storage.setItem(key, json, options);
+    return getStorage().setItem(key, json, options);
+  },
+  reload: function() {
+    getStorage(true);
   }
 };