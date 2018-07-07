f6ec16be9a696df2c1a0d9b7e3966f8f08bdc24a
Jessica Yao <jessicayao@Jessicas-MacBook-Air.local>
2018-07-07 12:22:07 -0500

    changed snakecase array

--- src/authentication/db-connection.js
+++ src/authentication/db-connection.js
@@ -3,6 +3,7 @@ import urljoin from 'url-join';
 import objectHelper from '../helper/object';
 import assert from '../helper/assert';
 import responseHandler from '../helper/response-handler';
+import paramArrays from '../helper/constants';
 
 function DBConnection(request, options) {
   this.baseOptions = options;
@@ -98,7 +99,7 @@ DBConnection.prototype.changePassword = function(options, cb) {
 
   body = objectHelper.merge(this.baseOptions, ['clientID']).with(options, ['email', 'connection']);
 
-  body = objectHelper.toSnakeCase(body, ['auth0Client']);
+  body = objectHelper.toSnakeCase(body, paramArrays.toSnakeCaseBaseParams);
 
   return this.request
     .post(url)
