86f109515f037b9615e0fcf0fb58c041cec8d365
Jessica Yao <jessicayao@Jessicas-MacBook-Air.local>
2018-07-07 11:31:26 -0500

    constants params

---
+++ src/helper/constants.js
@@ -0,0 +1,57 @@
+const constants = {
+  // alphabetical order
+  params: {
+    additional: {
+      access_type: 'access_type',
+      display: 'display'
+    },
+    auth0: {
+      audience: 'audience',
+      auth0Client: 'auth0Client',
+      connection: 'connection',
+      connection_scope: 'connection_scope',
+      _csrf: '_csrf',
+      device: 'device',
+      _intstate: '_intstate',
+      login_ticket: 'login_ticket',
+      owp: 'owp',
+      protocol: 'protocol',
+      realm: 'realm'
+    },
+    oauth2: {
+      acr_values: 'acr_values',
+      assertion: 'assertion',
+      claims: 'claims',
+      claims_locales: 'claims_locales',
+      client_assertion: 'client_assertion',
+      client_assertion_type: 'client_assertion_type',
+      client_id: 'client_id',
+      client_secret: 'client_secret',
+      code: 'code',
+      code_challenge: 'code_challenge',
+      code_challenge_method: 'code_challenge_method',
+      code_verifier: 'code_verifier',
+      display: 'display',
+      grant_type: 'grant_type',
+      id_token_hint: 'id_token_hint',
+      login_hint: 'login_hint',
+      max_age: 'max_age',
+      nonce: 'nonce',
+      password: 'password',
+      prompt: 'prompt',
+      ui_locales: 'ui_locales',
+      redirect_uri: 'redirect_uri',
+      registration: 'registration',
+      refresh_token: 'refresh_token',
+      request: 'request',
+      request_uri: 'request_uri',
+      response_mode: 'response_mode',
+      response_type: 'response_type',
+      scope: 'scope',
+      state: 'state',
+      username: 'username'
+    }
+  }
+};
+
+export default constants;
