9d4cca244d4ea3ca80e883f024fe4e23d4fd08d8
Vincent <vincent.desmares@gmail.com>
2020-05-29 09:56:38 +0200

    Fix IE default redirect url
    Applying changes from https://github.com/auth0/lock/pull/1373/files

--- src/helper/object.js
+++ src/helper/object.js
@@ -145,6 +145,9 @@ function getOriginFromUrl(url) {
     return undefined;
   }
   var parsed = getLocationFromUrl(url);
+  if (!parsed) {
+    return null;
+  }
   var origin = parsed.protocol + '//' + parsed.hostname;
   if (parsed.port) {
     origin += ':' + parsed.port;
