4ebdb770c8ef8e768e31d7b185807c3a08ec27fc
Steve Hobbs <steve.hobbs.mail@gmail.com>
2021-09-29 13:52:54 +0100

    Clean up the ci script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,9 +8,6 @@ MATCHER=${3:-"*"}
 NPM_NAME=$(node scripts/utils/attribute.js name)
 VERSION=$(node scripts/utils/attribute.js version)
 
-NPM_BIN=$(npm bin)
-STABLE=$($NPM_BIN/semver $VERSION -r "*")
-
 # Enable failing on exit status here because semver exits with 1 when the range
 # doesn't match.
 set -e
@@ -42,51 +39,6 @@ cdn_release()
   success "$NPM_NAME ($1) uploaded to cdn"
 }
 
-bower_release()
-{
-  # Check if tag exists
-  TAG_NAME="v$VERSION"
-  TAG_EXISTS=$(git tag -l "$TAG_NAME")
-
-  if [ ! -z "$TAG_EXISTS" ]; then
-    verbose "There is already a tag $TAG_EXISTS in git. Skipping git deploy."
-  else
-    verbose "Deploying $VERSION to git"
-
-    LAST_COMMIT=$(git log -1 --pretty=%B)
-    # removing dist and build folders from gitignore so it gets pushed to the tag
-    grep -v -e '^dist$' -e '^dist/$' .gitignore > /tmp/.gitignore
-    grep -v -e '^build$' -e '^build/$' .gitignore > /tmp/.gitignore
-    mv /tmp/.gitignore .gitignore
-    git add --force dist/*
-    git add --force build/*
-    git commit -am "$TAG_NAME"
-    git tag "$TAG_NAME" -m "$LAST_COMMIT"
-    git push origin $TAG_NAME
-    success "$NPM_NAME version ready for bower"
-  fi
-}
-
-npm_release()
-{
-  verbose "Checking if version $1 of $NPM_NAME is already available in npm…"
-
-  NPM_EXISTS=$(npm info -s $NPM_NAME@$1 version)
-
-  if [ ! -z "$NPM_EXISTS" ] && [ "$NPM_EXISTS" == "$1" ]; then
-    verbose "There is already a version $NPM_EXISTS in npm. Skipping npm publish…"
-  else
-    if [ ! -z "$STABLE" ]; then
-      verbose "Deploying $1 to npm"
-      npm publish
-    else
-      verbose "Deploying $1 to npm with tag $NPM_TAG"
-      npm publish --tag "$NPM_TAG"
-    fi
-    success "$NPM_NAME uploaded to npm registry"
-  fi
-}
-
 # Lint
 npm run lint
 
@@ -101,9 +53,5 @@ rm -rf build
 npm run build
 
 # Release
-git checkout -b dist
-bower_release
 new_line
 cdn_release "$VERSION"
-git checkout master
-git branch -D dist
