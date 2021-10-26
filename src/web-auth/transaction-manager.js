1556356816ae58e92f0b6d329ee49b7be1de0474
AJ Edelbrock <36162221+aedelbro@users.noreply.github.com>
2021-10-26 16:05:50 -0400

    make state expiration configurable

--- src/web-auth/transaction-manager.js
+++ src/web-auth/transaction-manager.js
@@ -9,6 +9,8 @@ function TransactionManager(options) {
   var transaction = options.transaction || {};
   this.namespace = transaction.namespace || DEFAULT_NAMESPACE;
   this.keyLength = transaction.keyLength || 32;
+  // Passed option is in minutes, convert to days
+  this.stateExpiration = options.stateExpiration ? (options.stateExpiration / 60 / 24) : times.MINUTES_30;
   this.storage = new Storage(options);
   this.options = options;
 }
@@ -68,7 +70,7 @@ TransactionManager.prototype.generateTransaction = function(
     }
 
     this.storage.setItem(this.namespace + state, transactionPayload, {
-      expires: times.MINUTES_30
+      expires: this.stateExpiration
     });
   }
 
