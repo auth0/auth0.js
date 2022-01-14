264289266e375f5756a3d3790c555b1492374ddd
Steve Hobbs <steve.hobbs@auth0.com>
2022-01-14 14:15:08 +0000

    Set sameSite to 'none' for cookies when using HTTPS (#1229)

--- test/helper/storage.cookie.test.js
+++ test/helper/storage.cookie.test.js
@@ -9,40 +9,40 @@ var cookieStorage = new CookieStorage();
 const KEY = 'foo';
 const VALUE = 'bar';
 
-describe('storage.cookies', function() {
-  beforeEach(function() {
-    sinon.stub(CookieLibrary, 'get').callsFake(function(key) {
+describe('storage.cookies', function () {
+  beforeEach(function () {
+    sinon.stub(CookieLibrary, 'get').callsFake(function (key) {
       expect(key).to.be(KEY);
       return VALUE;
     });
-    sinon.stub(CookieLibrary, 'set').callsFake(function(key, value) {
+    sinon.stub(CookieLibrary, 'set').callsFake(function (key, value) {
       expect(key).to.be(KEY);
       expect(value).to.be(VALUE);
     });
-    sinon.stub(CookieLibrary, 'remove').callsFake(function(key) {
+    sinon.stub(CookieLibrary, 'remove').callsFake(function (key) {
       expect(key).to.be(KEY);
     });
   });
-  afterEach(function() {
+  afterEach(function () {
     CookieLibrary.get.restore();
     CookieLibrary.set.restore();
     CookieLibrary.remove.restore();
   });
-  describe('getItem', function() {
-    it('calls Cookie.get', function() {
+  describe('getItem', function () {
+    it('calls Cookie.get', function () {
       const value = cookieStorage.getItem(KEY);
       expect(value).to.be(VALUE);
     });
   });
-  describe('removeItem', function() {
-    it('calls Cookie.remove', function(done) {
+  describe('removeItem', function () {
+    it('calls Cookie.remove', function (done) {
       cookieStorage.removeItem(KEY);
       done();
     });
   });
-  describe('setItem', function() {
-    beforeEach(function() {
-      sinon.stub(windowHandler, 'getWindow').callsFake(function() {
+  describe('setItem', function () {
+    beforeEach(function () {
+      sinon.stub(windowHandler, 'getWindow').callsFake(function () {
         return {
           location: {
             protocol: 'http:'
@@ -51,11 +51,11 @@ describe('storage.cookies', function() {
       });
     });
 
-    afterEach(function() {
+    afterEach(function () {
       windowHandler.getWindow.restore();
     });
 
-    it('calls Cookie.set with default values', function() {
+    it('calls Cookie.set with default values', function () {
       cookieStorage.setItem(KEY, VALUE);
 
       expect(CookieLibrary.set.firstCall.args).to.be.eql([
@@ -65,7 +65,7 @@ describe('storage.cookies', function() {
       ]);
     });
 
-    it('calls Cookie.set with custom values', function() {
+    it('calls Cookie.set with custom values', function () {
       cookieStorage.setItem(KEY, VALUE, { expires: 2, test: true });
 
       expect(CookieLibrary.set.firstCall.args).to.be.eql([
@@ -75,9 +75,9 @@ describe('storage.cookies', function() {
       ]);
     });
 
-    it('sets the secure flag on cookies when using the https protocol', function() {
+    it('sets the secure flag on cookies when using the https protocol', function () {
       windowHandler.getWindow.restore();
-      sinon.stub(windowHandler, 'getWindow').callsFake(function() {
+      sinon.stub(windowHandler, 'getWindow').callsFake(function () {
         return {
           location: {
             protocol: 'https:'
@@ -90,7 +90,7 @@ describe('storage.cookies', function() {
       expect(CookieLibrary.set.firstCall.args).to.be.eql([
         'foo',
         'bar',
-        { expires: 2, test: true, secure: true }
+        { expires: 2, test: true, secure: true, sameSite: 'none' }
       ]);
     });
   });
