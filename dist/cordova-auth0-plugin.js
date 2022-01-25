f2e06597e68b3eaec327436371c9a4d1a7bd8978
Steve Hobbs <steve.hobbs@auth0.com>
2022-01-25 11:37:52 +0000

    Release v9.19.0 (#1234)

--- dist/cordova-auth0-plugin.js
+++ dist/cordova-auth0-plugin.js
@@ -1,7 +1,7 @@
 /**
- * auth0-js v9.18.1
+ * auth0-js v9.19.0
  * Author: Auth0
- * Date: 2022-01-14
+ * Date: 2022-01-25
  * License: MIT
  */
 
@@ -11,7 +11,7 @@
   (global = global || self, global.CordovaAuth0Plugin = factory());
 }(this, (function () { 'use strict';
 
-  var version = { raw: '9.18.1' };
+  var version = { raw: '9.19.0' };
 
   var toString = Object.prototype.toString;
 
@@ -474,7 +474,7 @@
 
   	var symVal = 42;
   	obj[sym] = symVal;
-  	for (sym in obj) { return false; } // eslint-disable-line no-restricted-syntax, no-unreachable-loop
+  	for (sym in obj) { return false; } // eslint-disable-line no-restricted-syntax
   	if (typeof Object.keys === 'function' && Object.keys(obj).length !== 0) { return false; }
 
   	if (typeof Object.getOwnPropertyNames === 'function' && Object.getOwnPropertyNames(obj).length !== 0) { return false; }
@@ -492,7 +492,7 @@
   	return true;
   };
 
-  var origSymbol = typeof Symbol !== 'undefined' && Symbol;
+  var origSymbol = commonjsGlobal.Symbol;
 
 
   var hasSymbols = function hasNativeSymbols() {
@@ -974,24 +974,11 @@
   var booleanValueOf = Boolean.prototype.valueOf;
   var objectToString = Object.prototype.toString;
   var functionToString = Function.prototype.toString;
-  var $match = String.prototype.match;
-  var $slice = String.prototype.slice;
-  var $replace$1 = String.prototype.replace;
-  var $toUpperCase = String.prototype.toUpperCase;
-  var $toLowerCase = String.prototype.toLowerCase;
-  var $test = RegExp.prototype.test;
-  var $concat$1 = Array.prototype.concat;
-  var $join = Array.prototype.join;
-  var $arrSlice = Array.prototype.slice;
-  var $floor = Math.floor;
+  var match = String.prototype.match;
   var bigIntValueOf = typeof BigInt === 'function' ? BigInt.prototype.valueOf : null;
   var gOPS = Object.getOwnPropertySymbols;
   var symToString = typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol' ? Symbol.prototype.toString : null;
   var hasShammedSymbols = typeof Symbol === 'function' && typeof Symbol.iterator === 'object';
-  // ie, `has-tostringtag/shams
-  var toStringTag = typeof Symbol === 'function' && Symbol.toStringTag && (typeof Symbol.toStringTag === hasShammedSymbols ? 'object' : 'symbol')
-      ? Symbol.toStringTag
-      : null;
   var isEnumerable = Object.prototype.propertyIsEnumerable;
 
   var gPO = (typeof Reflect === 'function' ? Reflect.getPrototypeOf : Object.getPrototypeOf) || (
@@ -1002,30 +989,9 @@
           : null
   );
 
-  function addNumericSeparator(num, str) {
-      if (
-          num === Infinity
-          || num === -Infinity
-          || num !== num
-          || (num && num > -1000 && num < 1000)
-          || $test.call(/e/, str)
-      ) {
-          return str;
-      }
-      var sepRegex = /[0-9](?=(?:[0-9]{3})+(?![0-9]))/g;
-      if (typeof num === 'number') {
-          var int = num < 0 ? -$floor(-num) : $floor(num); // trunc(num)
-          if (int !== num) {
-              var intStr = String(int);
-              var dec = $slice.call(str, intStr.length + 1);
-              return $replace$1.call(intStr, sepRegex, '$&_') + '.' + $replace$1.call($replace$1.call(dec, /([0-9]{3})/g, '$&_'), /_$/, '');
-          }
-      }
-      return $replace$1.call(str, sepRegex, '$&_');
-  }
-
   var inspectCustom = require$$0.custom;
   var inspectSymbol = inspectCustom && isSymbol(inspectCustom) ? inspectCustom : null;
+  var toStringTag = typeof Symbol === 'function' && typeof Symbol.toStringTag !== 'undefined' ? Symbol.toStringTag : null;
 
   var objectInspect = function inspect_(obj, options, depth, seen) {
       var opts = options || {};
@@ -1052,12 +1018,8 @@
           && opts.indent !== '\t'
           && !(parseInt(opts.indent, 10) === opts.indent && opts.indent > 0)
       ) {
-          throw new TypeError('option "indent" must be "\\t", an integer > 0, or `null`');
+          throw new TypeError('options "indent" must be "\\t", an integer > 0, or `null`');
       }
-      if (has(opts, 'numericSeparator') && typeof opts.numericSeparator !== 'boolean') {
-          throw new TypeError('option "numericSeparator", if provided, must be `true` or `false`');
-      }
-      var numericSeparator = opts.numericSeparator;
 
       if (typeof obj === 'undefined') {
           return 'undefined';
@@ -1076,12 +1038,10 @@
           if (obj === 0) {
               return Infinity / obj > 0 ? '0' : '-0';
           }
-          var str = String(obj);
-          return numericSeparator ? addNumericSeparator(obj, str) : str;
+          return String(obj);
       }
       if (typeof obj === 'bigint') {
-          var bigIntStr = String(obj) + 'n';
-          return numericSeparator ? addNumericSeparator(obj, bigIntStr) : bigIntStr;
+          return String(obj) + 'n';
       }
 
       var maxDepth = typeof opts.depth === 'undefined' ? 5 : opts.depth;
@@ -1100,7 +1060,7 @@
 
       function inspect(value, from, noIndent) {
           if (from) {
-              seen = $arrSlice.call(seen);
+              seen = seen.slice();
               seen.push(from);
           }
           if (noIndent) {
@@ -1118,21 +1078,21 @@
       if (typeof obj === 'function') {
           var name = nameOf(obj);
           var keys = arrObjKeys(obj, inspect);
-          return '[Function' + (name ? ': ' + name : ' (anonymous)') + ']' + (keys.length > 0 ? ' { ' + $join.call(keys, ', ') + ' }' : '');
+          return '[Function' + (name ? ': ' + name : ' (anonymous)') + ']' + (keys.length > 0 ? ' { ' + keys.join(', ') + ' }' : '');
       }
       if (isSymbol(obj)) {
-          var symString = hasShammedSymbols ? $replace$1.call(String(obj), /^(Symbol\(.*\))_[^)]*$/, '$1') : symToString.call(obj);
+          var symString = hasShammedSymbols ? String(obj).replace(/^(Symbol\(.*\))_[^)]*$/, '$1') : symToString.call(obj);
           return typeof obj === 'object' && !hasShammedSymbols ? markBoxed(symString) : symString;
       }
       if (isElement(obj)) {
-          var s = '<' + $toLowerCase.call(String(obj.nodeName));
+          var s = '<' + String(obj.nodeName).toLowerCase();
           var attrs = obj.attributes || [];
           for (var i = 0; i < attrs.length; i++) {
               s += ' ' + attrs[i].name + '=' + wrapQuotes(quote(attrs[i].value), 'double', opts);
           }
           s += '>';
           if (obj.childNodes && obj.childNodes.length) { s += '...'; }
-          s += '</' + $toLowerCase.call(String(obj.nodeName)) + '>';
+          s += '</' + String(obj.nodeName).toLowerCase() + '>';
           return s;
       }
       if (isArray$1(obj)) {
@@ -1141,15 +1101,12 @@
           if (indent && !singleLineValues(xs)) {
               return '[' + indentedJoin(xs, indent) + ']';
           }
-          return '[ ' + $join.call(xs, ', ') + ' ]';
+          return '[ ' + xs.join(', ') + ' ]';
       }
       if (isError(obj)) {
           var parts = arrObjKeys(obj, inspect);
-          if ('cause' in obj && !isEnumerable.call(obj, 'cause')) {
-              return '{ [' + String(obj) + '] ' + $join.call($concat$1.call('[cause]: ' + inspect(obj.cause), parts), ', ') + ' }';
-          }
           if (parts.length === 0) { return '[' + String(obj) + ']'; }
-          return '{ [' + String(obj) + '] ' + $join.call(parts, ', ') + ' }';
+          return '{ [' + String(obj) + '] ' + parts.join(', ') + ' }';
       }
       if (typeof obj === 'object' && customInspect) {
           if (inspectSymbol && typeof obj[inspectSymbol] === 'function') {
@@ -1197,14 +1154,14 @@
           var ys = arrObjKeys(obj, inspect);
           var isPlainObject = gPO ? gPO(obj) === Object.prototype : obj instanceof Object || obj.constructor === Object;
           var protoTag = obj instanceof Object ? '' : 'null prototype';
-          var stringTag = !isPlainObject && toStringTag && Object(obj) === obj && toStringTag in obj ? $slice.call(toStr$1(obj), 8, -1) : protoTag ? 'Object' : '';
+          var stringTag = !isPlainObject && toStringTag && Object(obj) === obj && toStringTag in obj ? toStr$1(obj).slice(8, -1) : protoTag ? 'Object' : '';
           var constructorTag = isPlainObject || typeof obj.constructor !== 'function' ? '' : obj.constructor.name ? obj.constructor.name + ' ' : '';
-          var tag = constructorTag + (stringTag || protoTag ? '[' + $join.call($concat$1.call([], stringTag || [], protoTag || []), ': ') + '] ' : '');
+          var tag = constructorTag + (stringTag || protoTag ? '[' + [].concat(stringTag || [], protoTag || []).join(': ') + '] ' : '');
           if (ys.length === 0) { return tag + '{}'; }
           if (indent) {
               return tag + '{' + indentedJoin(ys, indent) + '}';
           }
-          return tag + '{ ' + $join.call(ys, ', ') + ' }';
+          return tag + '{ ' + ys.join(', ') + ' }';
       }
       return String(obj);
   };
@@ -1215,7 +1172,7 @@
   }
 
   function quote(s) {
-      return $replace$1.call(String(s), /"/g, '&quot;');
+      return String(s).replace(/"/g, '&quot;');
   }
 
   function isArray$1(obj) { return toStr$1(obj) === '[object Array]' && (!toStringTag || !(typeof obj === 'object' && toStringTag in obj)); }
@@ -1266,7 +1223,7 @@
 
   function nameOf(f) {
       if (f.name) { return f.name; }
-      var m = $match.call(functionToString.call(f), /^function\s*([\w$]+)/);
+      var m = match.call(functionToString.call(f), /^function\s*([\w$]+)/);
       if (m) { return m[1]; }
       return null;
   }
@@ -1366,10 +1323,10 @@
       if (str.length > opts.maxStringLength) {
           var remaining = str.length - opts.maxStringLength;
           var trailer = '... ' + remaining + ' more character' + (remaining > 1 ? 's' : '');
-          return inspectString($slice.call(str, 0, opts.maxStringLength), opts) + trailer;
+          return inspectString(str.slice(0, opts.maxStringLength), opts) + trailer;
       }
       // eslint-disable-next-line no-control-regex
-      var s = $replace$1.call($replace$1.call(str, /(['\\])/g, '\\$1'), /[\x00-\x1f]/g, lowbyte);
+      var s = str.replace(/(['\\])/g, '\\$1').replace(/[\x00-\x1f]/g, lowbyte);
       return wrapQuotes(s, 'single', opts);
   }
 
@@ -1383,7 +1340,7 @@
           13: 'r'
       }[n];
       if (x) { return '\\' + x; }
-      return '\\x' + (n < 0x10 ? '0' : '') + $toUpperCase.call(n.toString(16));
+      return '\\x' + (n < 0x10 ? '0' : '') + n.toString(16).toUpperCase();
   }
 
   function markBoxed(str) {
@@ -1395,7 +1352,7 @@
   }
 
   function collectionOf(type, size, entries, indent) {
-      var joinedEntries = indent ? indentedJoin(entries, indent) : $join.call(entries, ', ');
+      var joinedEntries = indent ? indentedJoin(entries, indent) : entries.join(', ');
       return type + ' (' + size + ') {' + joinedEntries + '}';
   }
 
@@ -1413,20 +1370,20 @@
       if (opts.indent === '\t') {
           baseIndent = '\t';
       } else if (typeof opts.indent === 'number' && opts.indent > 0) {
-          baseIndent = $join.call(Array(opts.indent + 1), ' ');
+          baseIndent = Array(opts.indent + 1).join(' ');
       } else {
           return null;
       }
       return {
           base: baseIndent,
-          prev: $join.call(Array(depth + 1), baseIndent)
+          prev: Array(depth + 1).join(baseIndent)
       };
   }
 
   function indentedJoin(xs, indent) {
       if (xs.length === 0) { return ''; }
       var lineJoiner = '\n' + indent.prev + indent.base;
-      return lineJoiner + $join.call(xs, ',' + lineJoiner) + '\n' + indent.prev;
+      return lineJoiner + xs.join(',' + lineJoiner) + '\n' + indent.prev;
   }
 
   function arrObjKeys(obj, inspect) {
@@ -1453,7 +1410,7 @@
           if (hasShammedSymbols && symMap['$' + key] instanceof Symbol) {
               // this is to prevent shammed Symbols, which are stored as strings, from being included in the string key section
               continue; // eslint-disable-line no-restricted-syntax, no-continue
-          } else if ($test.call(/[^\w$]/, key)) {
+          } else if ((/[^\w$]/).test(key)) {
               xs.push(inspect(key, obj) + ': ' + inspect(obj[key], obj));
           } else {
               xs.push(key + ': ' + inspect(obj[key], obj));
@@ -1785,7 +1742,6 @@
 
           i += 1;
           c = 0x10000 + (((c & 0x3FF) << 10) | (string.charCodeAt(i) & 0x3FF));
-          /* eslint operator-linebreak: [2, "before"] */
           out += hexTable[0xF0 | (c >> 18)]
               + hexTable[0x80 | ((c >> 12) & 0x3F)]
               + hexTable[0x80 | ((c >> 6) & 0x3F)]
@@ -1875,7 +1831,6 @@
   };
 
   var isArray$3 = Array.isArray;
-  var split = String.prototype.split;
   var push = Array.prototype.push;
   var pushToArray = function (arr, valueOrArray) {
       push.apply(arr, isArray$3(valueOrArray) ? valueOrArray : [valueOrArray]);
@@ -1912,8 +1867,6 @@
           || typeof v === 'bigint';
   };
 
-  var sentinel = {};
-
   var stringify = function stringify(
       object,
       prefix,
@@ -1933,23 +1886,8 @@
   ) {
       var obj = object;
 
-      var tmpSc = sideChannel$1;
-      var step = 0;
-      var findFlag = false;
-      while ((tmpSc = tmpSc.get(sentinel)) !== void undefined && !findFlag) {
-          // Where object last appeared in the ref tree
-          var pos = tmpSc.get(object);
-          step += 1;
-          if (typeof pos !== 'undefined') {
-              if (pos === step) {
-                  throw new RangeError('Cyclic object value');
-              } else {
-                  findFlag = true; // Break while
-              }
-          }
-          if (typeof tmpSc.get(sentinel) === 'undefined') {
-              step = 0;
-          }
+      if (sideChannel$1.has(object)) {
+          throw new RangeError('Cyclic object value');
       }
 
       if (typeof filter === 'function') {
@@ -1976,14 +1914,6 @@
       if (isNonNullishPrimitive(obj) || utils.isBuffer(obj)) {
           if (encoder) {
               var keyValue = encodeValuesOnly ? prefix : encoder(prefix, defaults.encoder, charset, 'key', format);
-              if (generateArrayPrefix === 'comma' && encodeValuesOnly) {
-                  var valuesArray = split.call(String(obj), ',');
-                  var valuesJoined = '';
-                  for (var i = 0; i < valuesArray.length; ++i) {
-                      valuesJoined += (i === 0 ? '' : ',') + formatter(encoder(valuesArray[i], defaults.encoder, charset, 'value', format));
-                  }
-                  return [formatter(keyValue) + '=' + valuesJoined];
-              }
               return [formatter(keyValue) + '=' + formatter(encoder(obj, defaults.encoder, charset, 'value', format))];
           }
           return [formatter(prefix) + '=' + formatter(String(obj))];
@@ -1998,7 +1928,7 @@
       var objKeys;
       if (generateArrayPrefix === 'comma' && isArray$3(obj)) {
           // we need to join elements in
-          objKeys = [{ value: obj.length > 0 ? obj.join(',') || null : void undefined }];
+          objKeys = [{ value: obj.length > 0 ? obj.join(',') || null : undefined }];
       } else if (isArray$3(filter)) {
           objKeys = filter;
       } else {
@@ -2006,9 +1936,9 @@
           objKeys = sort ? keys.sort(sort) : keys;
       }
 
-      for (var j = 0; j < objKeys.length; ++j) {
-          var key = objKeys[j];
-          var value = typeof key === 'object' && typeof key.value !== 'undefined' ? key.value : obj[key];
+      for (var i = 0; i < objKeys.length; ++i) {
+          var key = objKeys[i];
+          var value = typeof key === 'object' && key.value !== undefined ? key.value : obj[key];
 
           if (skipNulls && value === null) {
               continue;
@@ -2018,9 +1948,8 @@
               ? typeof generateArrayPrefix === 'function' ? generateArrayPrefix(prefix, key) : prefix
               : prefix + (allowDots ? '.' + key : '[' + key + ']');
 
-          sideChannel$1.set(object, step);
+          sideChannel$1.set(object, true);
           var valueSideChannel = sideChannel();
-          valueSideChannel.set(sentinel, sideChannel$1);
           pushToArray(values, stringify(
               value,
               keyPrefix,
@@ -2048,7 +1977,7 @@
           return defaults;
       }
 
-      if (opts.encoder !== null && typeof opts.encoder !== 'undefined' && typeof opts.encoder !== 'function') {
+      if (opts.encoder !== null && opts.encoder !== undefined && typeof opts.encoder !== 'function') {
           throw new TypeError('Encoder has to be a function.');
       }
 
@@ -2306,7 +2235,7 @@
               ) {
                   obj = [];
                   obj[index] = leaf;
-              } else if (cleanRoot !== '__proto__') {
+              } else {
                   obj[cleanRoot] = leaf;
               }
           }
