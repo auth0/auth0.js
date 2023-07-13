b20d8972e8b64a7fabe34f62562be3be837ab3d6
Frederik Prijck <frederik.prijck@auth0.com>
2023-07-13 23:21:16 +0200

    Release v9.22.0 (#1314)

--- dist/cordova-auth0-plugin.js
+++ dist/cordova-auth0-plugin.js
@@ -1,5 +1,5 @@
 /**
- * auth0-js v9.21.0
+ * auth0-js v9.22.0
  * Author: Auth0
  * Date: 2023-07-13
  * License: MIT
@@ -11,7 +11,7 @@
   (global = global || self, global.CordovaAuth0Plugin = factory());
 }(this, (function () { 'use strict';
 
-  var version = { raw: '9.21.0' };
+  var version = { raw: '9.22.0' };
 
   var toString = Object.prototype.toString;
 
