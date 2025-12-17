6b92e84db2293e23f8c32750de8837bcb2452e44
Amit Singh <amitk.singh@okta.com>
2025-12-17 12:36:54 +0530

    chore: refactor jsdocs.js to use ES module imports

--- scripts/jsdocs.js
+++ scripts/jsdocs.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-console */
+import { execSync } from 'child_process';
+import fs from 'fs';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const library = require('../package.json');
+
 if (process.platform === 'win32') {
   console.error('Must be run on a Unix OS');
   process.exit(1);
 }
-
-var library = require('../package.json');
-var execSync = require('child_process').execSync;
-var fs = require('fs');
 
 execSync('npm run jsdocs', { stdio: 'inherit' });
 if (fs.existsSync('docs')) {
