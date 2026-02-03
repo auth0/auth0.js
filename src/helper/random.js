d94888bb59b8a4a7fab24692e630d39d711dc04e
Amit Singh <amitk.singh@okta.com>
2026-02-03 13:03:03 +0530

    o handle invalid lengths and avoid modulo bias

--- src/helper/random.js
+++ src/helper/random.js
@@ -1,9 +1,10 @@
 import windowHelper from './window';
 
 function randomString(length) {
-  // eslint-disable-next-line
-  var bytes = new Uint8Array(length);
-  var result = [];
+  // Handle invalid length
+  if (!length || length <= 0) {
+    return '';
+  }
   var charset =
     '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._~';
 
@@ -13,10 +14,21 @@ function randomString(length) {
     return null;
   }
 
-  var random = cryptoObj.getRandomValues(bytes);
+  var result = [];
+  // Use rejection sampling to avoid modulo bias
+  // 256 % 66 = 58, so we reject values >= 198 (256 - 58)
+  var maxValid = 256 - (256 % charset.length);
+  while (result.length < length) {
+    // eslint-disable-next-line
+    var bytes = new Uint8Array(length - result.length);
+    var random = cryptoObj.getRandomValues(bytes);
 
-  for (var a = 0; a < random.length; a++) {
-    result.push(charset[random[a] % charset.length]);
+    for (var a = 0; a < random.length && result.length < length; a++) {
+      // Reject values that would cause modulo bias
+      if (random[a] < maxValid) {
+        result.push(charset[random[a] % charset.length]);
+      }
+    }
   }
 
   return result.join('');
--- test/helper/random.test.js
+++ test/helper/random.test.js
@@ -4,65 +4,73 @@ import sinon from 'sinon';
 import windowHelper from '../../src/helper/window';
 import random from '../../src/helper/random';
 
-describe('helpers random', function() {
-  describe('randomString with crypto', function() {
-    before(function() {
-      sinon.stub(windowHelper, 'getWindow').callsFake(function() {
+describe('helpers random', function () {
+  describe('randomString with crypto', function () {
+    before(function () {
+      sinon.stub(windowHelper, 'getWindow').callsFake(function () {
         return {
           crypto: {
-            getRandomValues: function() {
-              return [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+            getRandomValues: function (arr) {
+              // Fill the array with values 10-19 (all < 198, so no rejection)
+              for (var i = 0; i < arr.length; i++) {
+                arr[i] = 10 + (i % 10);
+              }
+              return arr;
             }
           }
         };
       });
     });
 
-    after(function() {
+    after(function () {
       windowHelper.getWindow.restore();
     });
 
-    it('return the a random string', function() {
+    it('return the a random string', function () {
       var string = random.randomString(10);
       expect(string).to.eql('ABCDEFGHIJ');
     });
   });
 
-  describe('randomString with msCrypto', function() {
-    before(function() {
-      sinon.stub(windowHelper, 'getWindow').callsFake(function() {
+  describe('randomString with msCrypto', function () {
+    before(function () {
+      sinon.stub(windowHelper, 'getWindow').callsFake(function () {
         return {
           msCrypto: {
-            getRandomValues: function() {
-              return [10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+            getRandomValues: function (arr) {
+              // Fill the array with values 10-19 (all < 198, so no rejection)
+              for (var i = 0; i < arr.length; i++) {
+                arr[i] = 10 + (i % 10);
+              }
+              return arr;
             }
           }
         };
       });
     });
 
-    after(function() {
+    after(function () {
       windowHelper.getWindow.restore();
     });
 
-    it('return the a random string', function() {
+    it('return the a random string', function () {
       var string = random.randomString(10);
       expect(string).to.eql('ABCDEFGHIJ');
     });
   });
 
-  describe('randomString without crypto', function() {
-    before(function() {
-      sinon.stub(windowHelper, 'getWindow').callsFake(function() {
+  describe('randomString without crypto', function () {
+    before(function () {
+      sinon.stub(windowHelper, 'getWindow').callsFake(function () {
         return {};
       });
     });
 
-    after(function() {
+    after(function () {
       windowHelper.getWindow.restore();
     });
 
-    it('return the a random string', function() {
+    it('return the a random string', function () {
       var string = random.randomString(10);
       expect(string).to.be(null);
     });
